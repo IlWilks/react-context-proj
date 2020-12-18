aa3080fb6ecde22532d0d8a0d760a6799bdb76ca
Ian <ianlloydwilkinson@gmail.com>
2020-12-18 16:51:26 -0700

    Home page benefits added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,52 @@ import { Container } from "semantic-ui-react"
 import { AccountContext } from "../providers/AccountProvider"
 
 export default () => {
-  const {username, membershipLevel} = useContext(AccountContext)
+  const {username, membershipLevel, accountPic} = useContext(AccountContext)
+
+  const switchRender = (param) => {
+    switch(param) {
+    case "Bronze" : return (
+      <p>A sincere thanks for your patronage</p>)
+    case "Silver" : return (
+      <>
+        <p>A sincere thanks for your patronage</p>
+        <p>A loving glance if you come to our office (address not provided)</p>
+      </>
+    )
+    case "Gold" : return (
+      <>
+        <p>A sincere thanks for your patronage</p>
+        <p>A loving glance if you come to our office (address not provided)</p>
+        <p>Your picture will be logged in our "Gold Member" filing cabinet</p>
+      </>
+    )
+    case "Platinum" : return (
+      <>
+        <p>A sincere thanks for your patronage</p>
+        <p>A loving glance if you come to our office (address not provided)</p>
+        <p>Your picture will be logged in our "Gold Member" filing Cabinet</p>
+        <p>Our CEO will personally use your money to wipe the crumbs from his face at dinner tonight</p>
+      </>
+    )
+  }
+}
+
   return (
     <Container>
-      <h1>Welcome, {username}</h1>
+      <div style={{...styles.flex}}>
+        <h1>Welcome, {username}</h1>
+        <img src={accountPic} width="auto" height="100px"/>     
+        </div>
       <h2>Your {membershipLevel} membership level qualifies you for the following benefits: </h2>
+      {switchRender(membershipLevel)}
     </Container>
   )
+}
+
+const styles = {
+  flex: { 
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
 }
--- src/providers/AccountProvider.js
+++ src/providers/AccountProvider.js
@@ -7,7 +7,7 @@ export const AccountConsumer = AccountContext.Consumer;
 class AccountProvider extends React.Component {
   state = {
     username: "Fooman77",
-    dateJoined: "12/18/18",
+    dateJoined: "12/18/20",
     membershipLevel: "Silver",
     accountPic: "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F025%2F177%2Fchrome_2018-01-12_16-47-56.jpg",
     updateAccount: (account) => this.updateAccount(account),
