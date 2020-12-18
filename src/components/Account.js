7fb6a26adea60c55bdcb69c2cc056d91903d2a02
Ian <ianlloydwilkinson@gmail.com>
2020-12-18 16:24:22 -0700

    picture changer added

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,11 +1,21 @@
-import React, { useContext } from "react"
-import { Card } from "semantic-ui-react"
+import React, { useContext, useState } from "react"
+import { Link } from "react-router-dom";
+import { Button, Card, Form, Icon } from "semantic-ui-react"
 import styled from "styled-components";
 import { AccountConsumer, AccountContext } from "../providers/AccountProvider"
 
 const Account = () => {
-  const {username, dateJoined, membershipLevel, accountPic} = useContext(AccountContext);
+  const [showForm, setShowForm] = useState(false)
+  const {username, dateJoined, membershipLevel, accountPic, updatePicture} = useContext(AccountContext);
+  const [text, setText] = useState('')
 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updatePicture(text)
+    setShowForm(false)
+  }
+  console.log(accountPic)
   return (
     <div style={{...styles.flex}}>
     <Card>
@@ -20,9 +30,27 @@ const Account = () => {
       </Card.Content>
     </Card>
     <Card>
+      <Card.Header style={{...styles.flexbutton }}>
+          <Button onClick={() => setShowForm(!showForm)} basic color='green' >
+            Click to change profile picture
+          </Button>
+      </Card.Header>
       <Card.Content>
-        {accountPic}
+        <img src={accountPic} height="auto" width= "100%"/>
       </Card.Content>
+      {showForm &&
+      <Card.Content extra>
+        <Form onSubmit={handleSubmit}> 
+          <Form.Input placeholder={accountPic}
+          label="New Picture"
+          type="text"
+          name="picture"
+          value={text}
+          onChange={(e) => setText(e.target.value)}/>
+          <Form.Button color="blue">Save</Form.Button>
+        </Form>
+      </Card.Content>
+}
     </Card>
     </div>
   )
@@ -36,4 +64,9 @@ const styles = {
     justifyContent: "space-between",
     alignItems: "center"
   },
+  flexbutton: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 }
--- src/providers/AccountProvider.js
+++ src/providers/AccountProvider.js
@@ -9,12 +9,17 @@ class AccountProvider extends React.Component {
     username: "Fooman77",
     dateJoined: "12/18/18",
     membershipLevel: "Silver",
-    accountPic: <img src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F025%2F177%2Fchrome_2018-01-12_16-47-56.jpg" height="auto" width= "100%"/>,
+    accountPic: "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F025%2F177%2Fchrome_2018-01-12_16-47-56.jpg",
     updateAccount: (account) => this.updateAccount(account),
+    updatePicture: (picture => this.updatePicture(picture)),
   };
 
   updateAccount = (account) => {
     this.setState({ ...account, });
+  }
+
+  updatePicture = (picture) => {
+    this.setState({ accountPic: picture });
   }
 
   render () {
