7b1c0f57dbf5859a721af8dc975ba579289615c0
Ian <ianlloydwilkinson@gmail.com>
2020-12-18 17:24:21 -0700

    tinkered with backgrounds

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar';
 import AccountProfile from './components/AccountProfile';
 import Home from './components/Home';
+import Particles from 'react-tsparticles';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Container>
+      <Container className= "test" id="welcome">
         <Switch>
           <Route
           exact
@@ -24,6 +25,86 @@ function App() {
           />
         </Switch>
       </Container>
+      <Particles id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#1f5bb5",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }} />
+      
     </>
   );
 }
